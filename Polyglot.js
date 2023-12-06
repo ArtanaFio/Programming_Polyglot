1ea76e934e06286984a74815c2e4f75cbd537815
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-12-06 16:26:49 -0500

    Edit JS code to alert webpages under construction
    Created variables to attach an event listener that displays a "webpage
    is under construction" note whenever either the CSS or JavaScript button
    is pressed. Also edited index.css to style the note to be smaller.

--- Polyglot.js
+++ Polyglot.js
@@ -3,13 +3,28 @@ const logo = document.querySelector('.logo');
 const longLogo = document.querySelector('.title-name');
 const container = document.querySelector('.logo-container');
 
+
 logo.addEventListener('mouseover', () => {
     longLogo.textContent = 'rogramming Polyglot';
 });
 
 logo.addEventListener('mouseleave', () => {
     longLogo.textContent = '';
-})
+});
 
 container.appendChild(logo);
 container.appendChild(longLogo);
+
+const content = document.querySelector('.content');
+const noButtons = document.querySelectorAll('.nonexistent');
+const note = document.querySelector('.note');
+
+noButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        note.textContent = "(Sorry, that webpage is under construction. Choose another option.)";
+    })
+});
+
+content.appendChild(note);
+
+
